05133381a462176d26980086abac5acafba83f0e
szkan <71358350+szkan@users.noreply.github.com>
2020-10-28 14:20:30 +0900

    出品機能

---
+++ app/models/product.rb
@@ -0,0 +1,8 @@
+class Product < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :image, presence: true
+end
